ff65ee8cd9a481794118503a421aeab9e7a1c95b
stefania11 <druga.stefania@gmail.com>
2015-09-26 16:19:08 -0400

    generating users/ sessions / home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  devise_for :users
+  root to: "home#index"
+  # game :ressources 
+  # get 'sandbox' => 'game#sandbox'
+  # get 'matching' => 'game#matching'
+  # get 'rythm' => 'game#rythms'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
